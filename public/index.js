1a93f858ba68c35b815d33cc08fa2714f3e35a3e
d-sachi <deepthisachi@yahoo.com>
2021-03-07 20:49:38 +0530

    profile page frontend

---
+++ public/index.js
@@ -0,0 +1,5 @@
+function editprofiledesc(){
+	document.getElementById("profile-desc-text").innerHTML = '<form method="post" id="profileDescEdit">'
+		+ document.getElementById("profile-desc-text").innerHTML + '</form>';
+	// var inputdesc = document.getElementsByClassName("petdesc");
+}
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
+
+// Bootstrap
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+
 app.use(express.static(__dirname + '/public'));
 app.listen(port, () => {
   console.log('Listening on *:3000');
