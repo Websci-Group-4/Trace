769032c864b6081f78e79637e8d97618ccfd38e6
Jacob Dyer <dyerj@rpi.edu>
2021-03-10 02:10:38 -0500

    Project Reorganization
    Moved some files around and updated our package.json using 'npm init' to best reflect our project at this time.

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-const express = require('express');
-const path = require('path')
-const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-})
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
 
 // Bootstrap
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
+// Load in external directories.
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/images'));
+
+// Fundamental Route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+})
+
 app.listen(port, () => {
   console.log('Listening on *:3000');
-})
+});
