8c0176f20382ae3b837d2f6c00a6f81d7950d765
Jacob Dyer <dyerj@rpi.edu>
2021-03-11 18:43:19 -0500

    Stripped Imgur
    It turns out we're not going to need it given the scope of this project.  Not now, not ever.
    
    Co-Authored-By: Ethan Whitton <45616121+TheStopsign@users.noreply.github.com>

--- server.js
+++ server.js
@@ -15,14 +15,10 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/images'));
 
-// ---- PREPARE API ----- //
-
-// Load in our configuration data for anything that needs it.
-var config_filesync = fs.readFileSync('config.json');
-var config_json = JSON.parse(config_filesync);
-
-// Get client_id for our Imgur Application.
-const IMGUR_ID = config_json.imgur.clientid;
+// Fundamental Route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 // ----- BEGIN API ------ //
 
@@ -30,24 +26,6 @@ const IMGUR_ID = config_json.imgur.clientid;
 
 // ------ END API ------- //
 
-// Fundamental Route
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-// Imgur Authorization Route handling.
-app.get('/imgur', function(req, res){
-  // Compose the Imgur authorization link and redirect the user to that instead.
-  var auth_link = config_json.imgur.auth_endpoint;
-    auth_link += "?client_id=" + IMGUR_ID;
-    auth_link += "&response_type=" + config_json.imgur.response_type;
-  res.redirect(auth_link);
-});
-
-// Listen at localhost:3000.
 app.listen(port, () => {
   console.log('Listening on *:3000');
-
-  // Run status checks.
-  if(IMGUR_ID) console.log("Status - Imgur: Ready!");
 });
