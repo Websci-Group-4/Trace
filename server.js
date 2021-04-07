39f7583fdb3c353de0267bd258dc1412a303caa8
Jacob Dyer <dyerj@rpi.edu>
2021-04-06 23:38:24 -0400

    Implemented Mongoose + DB Connection
    Emails already sent.  Starts up right, closes properly.

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const http = require('http').Server(app);
 const fs = require('fs');
 const cors = require('cors');
 
+const mongoose = require('mongoose');
+
+const nodeCleanup = require('node-cleanup');
+
 // ======================================================================
-// GETTING SEVER CONFIGURATION
+// GET SEVER CONFIGURATION
 // ======================================================================
 
 const configPath = 'config.json';
@@ -17,14 +21,11 @@ console.log(`\nRetrieving configuration data from '${configPath}'...`);
 var config = JSON.parse(fs.readFileSync(configPath, configEncoding));
 console.log(`Retrieval of configuration data from '${configPath}' SUCCESS.`);
 
-// Set up the server according to configuration data.
-const PORT = config.server.port;
-const DIRECTORIES = config.server.static_directories;
-const ROUTERS = config.server.routers;
+// ======================================================================
+// SET UP STATIC DIRECTORIES
+// ======================================================================
 
-// ======================================================================
-// STATIC DIRECTORY SETUP
-// ======================================================================
+const DIRECTORIES = config.server.static_directories;
 
 // Serve all listed static directories with Express.
 console.log("\nServing static directories...");
@@ -38,14 +39,18 @@ for(var i = 0; i < DIRECTORIES.length; i++) {
 console.log("Serving static directories SUCCESS.");
 
 // ======================================================================
-// ROUTING SETUP
+// SET UP ROUTES
 // ======================================================================
+
+const ROUTERS = config.server.routers;
 
 app.use(cors()); // Open CORS policy.
 
 // Set up our landing page route.
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+  // Redirect to Angular's serving the site since that's
+  // where we will use Angular's routing in our project.
+  res.redirect('http://localhost:4200/');
 });
 
 // Get all named routers and set them to their relevant paths.
@@ -59,10 +64,56 @@ for(var i = 0; i < ROUTERS.length; i++) {
 console.log("Loading routers SUCCESS.");
 
 // ======================================================================
-// SERVER SETUP
+// CONNECT TO DATABASE
 // ======================================================================
 
-// Start the server.
-var server = app.listen(PORT, function() {
-  console.log("\nServer up on *:" + PORT.toString());
+const DBURI = config.server.dbURI;
+const DBNAME = config.server.dbName;
+
+// Attempt to connect to the database.
+console.log(`\nConnecting to database '${DBNAME}' with Mongoose...`);
+mongoose.connect(DBURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+// ======================================================================
+// SET UP AND SERVE THE SERVER
+// ======================================================================
+
+const PORT = config.server.port;
+
+// Application starts if Node connects to our database successfully.
+db.once('open', function() {
+  console.log(`Mongoose connection to database '${DBNAME}' SUCCESS.`);
+
+  // Start the server.
+  var server = app.listen(PORT, function() {
+    console.log("\nServer up on *:" + PORT.toString());
+  });
+
+  // Set up functionality to close the client connection when the server is stopped.
+  nodeCleanup(function() {
+    console.log("\nRunning shutdown process...");
+    db.close(function(error) {
+      if(error) {
+        console.log(error);
+      } else {
+        console.log(`Closed Mongoose connection to '${DBNAME}'.`);
+      }
+    });
+    server.close(function() {
+      console.log("Server closed.");
+    });
+    nodeCleanup.uninstall(); // Cleanup function need only be run once.
+    return false; // Allows time to perform asynchronous cleanup. 
+  });
+});
+
+// Application fails if Node fails to connect to our database.
+db.on('error', function() {
+  console.log(`Mongoose connection to database '${DBNAME}' FAILED.`);
+  throw new Error(`Unable to connect to MongoDB database '${DBNAME}'.`);
 });
