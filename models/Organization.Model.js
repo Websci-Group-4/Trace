52f31f1e9a43575e3a41b9cf8b574bdd325e391f
TheStopsign <ejwhitton43@gmail.com>
2021-04-08 16:19:09 -0400

    added base model files structured for later routing

---
+++ models/Organization.Model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Organization = new Schema({
+	name: {
+        type: String,
+        required: true
+    },
+    baseUrls: [{
+        type: String,
+        required: true
+    }],
+    users: [{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }]
+});
+
+module.exports = mongoose.model("Organization", Organization);
