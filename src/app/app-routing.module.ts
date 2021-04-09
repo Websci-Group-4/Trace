98193517b635d3ae13f8c087577ec2c0d3fb1976
d-sachi <deepthisachi@yahoo.com>
2021-04-09 17:13:28 -0400

    html+css to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from "./homepage/homepage.component";
+import { AboutComponent } from "./about/about.component";
+import { ImageComponent } from "./image/image.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { UploadComponent } from "./upload/upload.component";
+import { SigninComponent } from './signin/signin.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	 {path: 'home', component: HomepageComponent},
+	 {path: 'about', component: AboutComponent},
+	 {path: 'image', component: ImageComponent},
+	 {path: 'profile', component: ProfileComponent},
+	 {path: 'upload', component: UploadComponent},
+	 {path: 'signin', component: SigninComponent},
+	 {path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
